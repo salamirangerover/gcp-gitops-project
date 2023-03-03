b4ab36e743ec779cfaa41b36c8dd7b4fc847f346
salamirangerover <salamirangerover@gmail.com>
2023-03-03 14:04:45 +0000

    edit .gitignore and terraform/vars.tf

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -3,7 +3,7 @@ variable "project_id" {}
 variable "cluster_name" {
   type        = string
   description = "cluster name"
-  default     = "k8s-cluster"
+  default     = "salamirangerover-cluster"
 }
 
 variable "region" {
