b283ae09ad6dc2532478c5ffa5120df1f69190d1
gleb <gleb.malko@hoster.by>
2023-03-05 00:32:26 +0300

    rm nginx

--- terraform/main.tf
+++ terraform/main.tf
@@ -86,20 +86,6 @@ resource "kubectl_manifest" "argocd" {
   override_namespace = "argocd"
 }
 
-### nginx_app 
-
-data "kubectl_file_documents" "my-nginx-app" {
-  content = file("../manifests/argocd/my-nginx-app.yaml")
-}
-
-resource "kubectl_manifest" "my-nginx-app" {
-  depends_on = [
-    kubectl_manifest.argocd,
-  ]
-  count              = length(data.kubectl_file_documents.my-nginx-app.documents)
-  yaml_body          = element(data.kubectl_file_documents.my-nginx-app.documents, count.index)
-  override_namespace = "argocd"
-}
 
 ### wordpress_app
 
