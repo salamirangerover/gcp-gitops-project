b0b242216671e67a5ad477abc70e7f457ab7ba1a
gleb <gleb.malko@hoster.by>
2023-03-06 18:03:06 +0300

    add google_project_iam_binding to main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,6 +19,23 @@ resource "google_service_account" "main" {
   display_name = "GKE Cluster ${var.cluster_name} Service Account"
 }
 
+resource "google_project_iam_binding" "project" {
+  for_each = toset([
+    "roles/logging.logWriter",
+    "roles/monitoring.metricWriter",
+    "roles/monitoring.viewer",
+    "roles/stackdriver.resourceMetadata.writer",
+    "roles/dns.admin"
+  ])
+
+  project = var.project_id
+  role    = each.key
+  members = [
+    "serviceAccount:${google_service_account.main.email}"
+  ]
+}
+
+
 resource "google_container_cluster" "main" {
   name               = var.cluster_name
   location           = var.location
