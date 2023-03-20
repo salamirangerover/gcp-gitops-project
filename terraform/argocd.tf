fdd79e933a2e7329808622117ac5d4163ae16356
gleb <gleb.malko@hoster.by>
2023-03-20 12:18:30 +0300

    restructure: ingress-nginx

--- terraform/argocd.tf
+++ terraform/argocd.tf
@@ -1,47 +1,3 @@
-# provider "kubectl" {
-#   host                   = module.gke_auth.host
-#   cluster_ca_certificate = module.gke_auth.cluster_ca_certificate
-#   token                  = module.gke_auth.token
-#   load_config_file       = false
-# }
-
-### argocd 
-
-# data "kubectl_file_documents" "namespace" {
-#   content = file("../manifests/argocd/namespace.yaml")
-# }
-
-# data "kubectl_file_documents" "argocd" {
-#   content = file("../manifests/argocd/install.yaml")
-# }
-
-# data "kubectl_file_documents" "ingress" {
-#   content = file("../manifests/argocd/ingress.yaml")
-# }
-
-# resource "kubectl_manifest" "ingress" {
-#   count              = length(data.kubectl_file_documents.ingress.documents)
-#   yaml_body          = element(data.kubectl_file_documents.ingress.documents, count.index)
-#   override_namespace = "argocd"
-# }
-
-
-# resource "kubectl_manifest" "namespace" {
-#   count              = length(data.kubectl_file_documents.namespace.documents)
-#   yaml_body          = element(data.kubectl_file_documents.namespace.documents, count.index)
-#   override_namespace = "argocd"
-# }
-
-# resource "kubectl_manifest" "argocd" {
-#   depends_on = [
-#     kubectl_manifest.namespace,
-#   ]
-#   count              = length(data.kubectl_file_documents.argocd.documents)
-#   yaml_body          = element(data.kubectl_file_documents.argocd.documents, count.index)
-#   override_namespace = "argocd"
-# }
-
-### init_monitoring_app
 resource "helm_release" "argocd" {
 
   name = "argocd"
@@ -51,14 +7,9 @@ resource "helm_release" "argocd" {
   namespace        = "argocd"
   create_namespace = true
 
-
-  # values = [
-  #   file("manifests/init_app.yaml")
-  # ]
-
 }
 
-
+### init_argocd_app
 data "kubectl_file_documents" "init_app" {
   content = file("../manifests/_init/init_app.yaml")
 }
